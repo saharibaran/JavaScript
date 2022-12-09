b9c4eb71c0d2bc5a5f7e656f896efc7fdbee43e1
Sahar Baran <112083611+saharibaran@users.noreply.github.com>
2022-12-09 10:29:17 +0200

    Add files via upload

---
+++ Module_3/Ex1/1.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const target = document.getElementById('target');
+target.setAttribute('class', 'my-list');
+target.innerHTML += "<li>First item</li>";
+target.innerHTML += "<li>Second item</li>";
+target.innerHTML += "<li>Third item</li>";
+
