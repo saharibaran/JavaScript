474845403372d70e0df1408f5173874b022d07fd
Sahar Baran <112083611+saharibaran@users.noreply.github.com>
2022-12-09 10:26:33 +0200

    Delete Module_3/Ex1 directory

--- Module_3/Ex1/1.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const target = document.getElementById('target');
-target.setAttribute('class', 'my-list');
-target.innerHTML += "<li>First item</li>";
-target.innerHTML += "<li>Second item</li>";
-target.innerHTML += "<li>Third item</li>";
-
